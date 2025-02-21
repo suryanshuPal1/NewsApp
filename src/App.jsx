e898a84239ed3b0b66ec048c22a684bc7e2d6279
ashvin <ashvinghugare1@gmail.com>
2025-02-21 16:46:05 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
-import './App.css'
 
-const App = () => {
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './Components/Navbar/Navbar';
+import Logo from './Components/Logo/Logo';
+import Footer from './Components/Footer/Footer';
+import Hero from './Pages/Hero/Hero';
+import Technology from './Pages/Technology/Technology';
+import TopVideos from './Pages/TopVideos/TopVideos';
+import TopNews from './Pages/TopNews/TopNews';
+import DailyReports from './Pages/DailyReports/DailyReports';
+
+function App() {
+
   return (
-    <div>App</div>
+    <div className="bg-gray-100 min-h-screen">
+    <Router>
+      <Logo />
+      <Navbar />
+      <Hero />
+        <DailyReports />
+        <Technology />  
+        <TopVideos />
+        <TopNews />
+      <Footer />
+   </Router>
+   
+
+       
+</div>
   )
 }
 
