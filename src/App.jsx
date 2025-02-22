4267a7241b010094f1bf0573a7fbdf0fc3952518
suryanshuPal1 <suryanshupal7@gmail.com>
2025-02-22 10:55:50 +0530

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,6 @@ const App = () => {
       <Signup/>
       <Verify/>
       <Vertification/>
-      
     </>
   )
 }
--- src/components/CustomizePreferenceButton/Button.jsx
+++ src/components/CustomizePreferenceButton/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = ({title,img}) => {
   return (
-    <div className='hover:bg-[#101450] hover:text-white px-2 border rounded-sm m-1  min-w-20'>
+    <div className='hover:bg-[#101450] hover:text-white px-2 border rounded m-1  min-w-20'>
         <button className='flex flex-row justify-center items-center p-1'>
           <span className='pr-4'>
             <img src={img} alt="" />
