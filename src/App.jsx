ecacccab7a711cc85f00ec602c71628f1fcc10ae
deepak <deekus1140@gmail.com>
2025-02-22 14:13:45 +0530

    Upade

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import ForgetPassword from './authpages/ForgetPassword'
 import PassVerification from './authpages/PassVerification'
 import ResetPass from './authpages/ResetPass'
 import PassResetSuccessfully from './authpages/PassResetSuccessfully'
+import Phone from './authpages/Phone'
+import Email from './authpages/Email'
 
 const App = () => {
   return (
@@ -14,6 +16,8 @@ const App = () => {
         <PassVerification/>
         <ResetPass/>
         <PassResetSuccessfully/>
+        <Phone/>
+        <Email/>
     </>
   )
 }
