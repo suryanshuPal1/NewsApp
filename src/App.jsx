f23efd5189e57abff65e422ea2839216374020e4
suryanshuPal1 <suryanshupal7@gmail.com>
2025-02-24 10:43:08 +0530

    login page active

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,53 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import Navbar from './Components/Navbar/Navbar';
+// import Logo from './Components/Logo/Logo';
+// import Footer from './Components/Footer/Footer';
+// import MainHomePage from './Pages/MainHomePage';
+// import Budget from './Components/Home/Budget';
+// import Technology from './PagesOfHome/Technology';
+
+// import './App.css';
+// import DailyReport from './PagesOfHome/DailyReport';
+// import EditorPack from './PagesOfHome/EditorPack';
+// import MostRead from './PagesOfHome/MostRead';
+// import TopNews from './PagesOfHome/TopNews';
+// import Crime from './Components/Home/Crime';
+// import InnerCrime from './Components/Home/InnerCrime';
+// import Login from './authpages/authLoginPages/Login' 
+
+
+// function App() {
+//   return (
+//       <div className="bg-gray-100 min-h-screen">
+//         <Logo />
+//         <Navbar />
+
+//         <Routes>
+//           {/* Pages of Navbar */}
+//           <Route path="/" element={<MainHomePage />} />
+//           <Route path="/log-in" element={<Login />} />
+
+
+//           {/* pages of home */}
+//           <Route path="/technology" element={<Technology />}/>
+//           <Route path="/daily-report" element={<DailyReport />}/>
+//           <Route path="/editor-pack" element={<EditorPack />}/>
+//           <Route path="/most-read" element={<MostRead />}/>
+//           <Route path="/top-news" element={<TopNews />}/>
+//           <Route path="/crime" element={<Crime />} />
+//           <Route path="/innercrime" element={<InnerCrime />} />
+//           <Route path="/budget-2025" element={<Budget />} />
+
+//         </Routes>
+
+//         <Footer/>
+//       </div>
+//   );
+// }
+
+// export default App;
+
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Navbar from './Components/Navbar/Navbar';
 import Logo from './Components/Logo/Logo';
 import Footer from './Components/Footer/Footer';
@@ -13,34 +62,37 @@ import MostRead from './PagesOfHome/MostRead';
 import TopNews from './PagesOfHome/TopNews';
 import Crime from './Components/Home/Crime';
 import InnerCrime from './Components/Home/InnerCrime';
-import Budget from './Components/Home/Budget';
+import Login from './authpages/authLoginPages/Login';
 
 function App() {
+  const location = useLocation(); // Get current route
+  
+  const isLoginPage = location.pathname === "/log-in"; // if login page is on
+
   return (
-      <div className="bg-gray-100 min-h-screen">
-        <Logo />
-        <Navbar />
+    <div className="bg-gray-100 min-h-screen">
+      {!isLoginPage && <Logo />}
+      {!isLoginPage && <Navbar />}
 
-        <Routes>
-          {/* Pages of Navbar */}
-          <Route path="/" element={<MainHomePage />} />
-          <Route path="/budget-2025" element={<Budget />} />
+      <Routes>
+        {/* Login Page */}
+        <Route path="/log-in" element={<Login />} />
 
+        {/* Other Pages */}
+        <Route path="/" element={<MainHomePage />} />
+        <Route path="/technology" element={<Technology />} />
+        <Route path="/daily-report" element={<DailyReport />} />
+        <Route path="/editor-pack" element={<EditorPack />} />
+        <Route path="/most-read" element={<MostRead />} />
+        <Route path="/top-news" element={<TopNews />} />
+        <Route path="/crime" element={<Crime />} />
+        <Route path="/innercrime" element={<InnerCrime />} />
+        <Route path="/budget-2025" element={<Budget />} />
+      </Routes>
 
-          {/* pages of home */}
-          <Route path="/technology" element={<Technology />}/>
-          <Route path="/daily-report" element={<DailyReport />}/>
-          <Route path="/editor-pack" element={<EditorPack />}/>
-          <Route path="/most-read" element={<MostRead />}/>
-          <Route path="/top-news" element={<TopNews />}/>
-          <Route path="/crime" element={<Crime />} />
-          <Route path="/innercrime" element={<InnerCrime />} />
-          <Routes path="/budget-2025" element={<Budget />} />
-
-        </Routes>
-
-        <Footer/>
-      </div>
+      {/* Hide Footer when on Login Page */}
+      {!isLoginPage && <Footer />}
+    </div>
   );
 }
 
