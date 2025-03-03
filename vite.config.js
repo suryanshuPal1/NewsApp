ca763b3afa81c640f2c79c7007638365913ef6be
deepak <deekus1140@gmail.com>
2025-03-03 13:51:31 +0530

    Fixed image import paths and updated components

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,26 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5173, // Or remove this line to use the default.
-  },
+export default defineConfig(({ mode }) => {
+ 
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    define: {
+      // Expose the base API URL to your client-side code via process.env.REACT_APP_BASE_API_URL
+      'process.env.REACT_APP_BASE_API_URL': JSON.stringify(env.VITE_BASE_API_URL),
+    },
+    plugins: [react()],
+    server: {
+      port: 5173, 
+      proxy: {
+
+        '/api': {
+          target: env.VITE_BASE_API_URL,
+          changeOrigin: true,
+          secure: false,
+        },
+      },
+    },
+  };
 });
